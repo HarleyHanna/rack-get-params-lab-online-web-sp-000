0bda15c31f1595620ad7a65be6e3a6d36744c614
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-24 20:24:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty."
       else
-        
+        @@cart.each do 
       end
     elsif req.path.match(/add/)
       
